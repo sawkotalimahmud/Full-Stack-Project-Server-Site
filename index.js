4de7586459f67916639fbfa9131446ddfc41b02d
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-22 14:34:35 -0700

    Connected With Database

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.POST || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kpfwg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+async function run (){
+  try{
+    await client.connect();
+    console.log('database cannected');
+  }
+  finally{
+
+  }
+}
+run().catch(console.dri)
 
 
 app.get('/', (req, res) => {
