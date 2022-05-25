caafd627d3de13e79589864a10fbbb5712435b9c
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-25 02:31:16 -0700

    Show detail by id it done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.POST || 5000;
 
@@ -18,12 +18,22 @@ async function run (){
   try{
     await client.connect();
     const productsCollection = client.db("assignment-12").collection("products");
+
+    // All Products Api
     app.get('/products', async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
-    })
+    });
+
+    // Get Single Product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
   }
   finally{
 
