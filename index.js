892ece7469b026e36173dc5004254e97335ac279
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-27 08:33:02 -0700

    VarifyAdmin and make admin API done

--- index.js
+++ index.js
@@ -15,12 +15,38 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run (){
   try{
     await client.connect();
     const productsCollection = client.db("assignment-12").collection("products");
     const ordersCollection = client.db("assignment-12").collection("orders");
     const usersCollection = client.db("assignment-12").collection("users");
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    }
 
     // All Products Api
     app.get('/products', async (req, res) => {
@@ -50,8 +76,16 @@ async function run (){
       res.send({result, token});
     });
 
+    // Verify Admin
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin});
+    });
+
     // Make Admin Api
-    app.put('/user/admin/:email', async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = {email: email};
       const updateDoc = {
